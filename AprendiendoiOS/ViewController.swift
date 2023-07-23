e589f872d2646d64c140936c3d773e5525cdac92
Miguel <migueljomartinez@gmail.com>
2023-07-22 23:42:23 -0500

    Validating form fields

--- AprendiendoiOS/ViewController.swift
+++ AprendiendoiOS/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func loginButtonAction() {
+        let email = self.emailTextField.text
+        let password = self.passwordTextField.text
+        
+        if let emailIsEmpty = email?.isEmpty, let passwordIsEmpty = password?.isEmpty {
+            if emailIsEmpty || passwordIsEmpty {
+                print("Email and password are required")
+                return
+            }
+        }
+        
+        if email == "m@m.com" && password == "123" {
+            print("Welcome!")
+        } else {
+            print("Invalid credentials!")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
